e6e8ef4cc80f699fd331d118566f946b16942187
g.s.singh <g.s.singh@DDC5-DX-2ZF8H92.dir.svc.accenture.com>
2018-12-04 12:44:28 +0530

    first commit setup

---
+++ ResourceManagment/src/main/java/com/channa/res/ResourceManagmentApplication.java
@@ -0,0 +1,12 @@
+package com.channa.res;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceManagmentApplication.class, args);
+	}
+}
